165241c080db22d80d2992139ebed65f183a11ec
kaiser shahid <kaisershahid@gmail.com>
2022-10-23 00:56:09 -0400

    - state.store: added getBatchDispatcher() + related logic; cleaned up

--- src/state/store.spec.ts
+++ src/state/store.spec.ts
@@ -1,9 +1,9 @@
-import { StoreImpl } from "./store"
+import { StoreEventPayload, StoreImpl } from "./store"
 
 describe('package: state', () => {
     describe('class StoreImpl', () => {
         type State1 = {k1: string, k2: number};
-        type State2 = {k3: boolean};
+        type State2 = {k3: any};
 
         const store = new StoreImpl<{s1: State1, s2: State2}>({
             s1: {k1: 'true', k2: 1},
@@ -40,27 +40,67 @@ describe('package: state', () => {
 
         it('handles dispatched action and emits event, then unsubscribes', () => {
             let event: any = {};
-            const unsub = store.listenFor('s1', (stateId, action, states, prevState) => {
-                event = {stateId, action, states, prevState};
+            let called = 0;
+            const unsub = store.listenFor('s1', (eventId, {eventStamp, stateId, action, states, prevState}: Partial<StoreEventPayload>) => {
+                event = {eventId, eventStamp, stateId, action, states, prevState};
+                called++;
             });
 
             const dispatcher = store.getDispatcher('s1');
             
             // @assert: action is handled by reducer and updates s1
             const action1 = {_action: 's1.replace', k1: 'v', k2: 0};
+            
             dispatcher(action1)
-            expect(event).toEqual(expect.objectContaining({
+            const {eventStamp, ...restEvent} = event;
+            expect(restEvent).toEqual(expect.objectContaining({
+                eventId: 's1',
                 stateId: 's1',
-                action: action1
+                action: action1,
+                states: {
+                    s1: {
+                        k1: 'v',
+                        k2: 0
+                    },
+                    s2: {
+                        k3: false
+                    }
+                },
+                prevState: {
+                    k1: 'true',
+                    k2: 1
+                }
             }));
+            expect(eventStamp).toMatch(/1-/);
 
             // @assert: successfully unsubscribes from state change
             unsub();
             dispatcher({_action: 's1.replace', k1: 'Z', k2: 9});
-            expect(event).toEqual(expect.objectContaining({
-                stateId: 's1',
-                action: action1
-            }));
+            expect(called).toEqual(1);
+        })
+
+        it('handles batch actions', () => {
+            let events: any[] = [];
+            let called = 0;
+            const unsub1 = store.listenFor('s1', (eventId, {eventStamp, stateId, action, states, prevState}: Partial<StoreEventPayload>) => {
+                events.push({eventId, eventStamp, stateId, action, states, prevState});
+                called++;
+            });
+            const unsub2 = store.listenFor('s2', (eventId, {eventStamp, stateId, action, states, prevState}: Partial<StoreEventPayload>) => {
+                events.push({eventId, eventStamp, stateId, action, states, prevState});
+                called++;
+            });
+
+            store.getBatchDispatcher()([
+                { stateId: 's1', action: {_action: 's1.replace', k1: 'a', k2: 0}},
+                { stateId: 's1', action: {_action: 's1.replace', k1: 'b', k2: 1}},
+                { stateId: 's2', action: {_action: 's2.replace', k3: 'c' }}
+            ])
+
+            expect(events.length).toEqual(2);
+
+            unsub1();
+            unsub2();
         })
     })
 })
--- src/state/store.ts
+++ src/state/store.ts
@@ -9,8 +9,23 @@ export type ActionBatch<Type = Record<string,any>> = {stateId: string, action: A
  * If modifications occur, **you must return a new object**, otherwise, changes will be ignored.
  */
 export type Reducer<Type = any> = (stateId: string, action: any, states: Type) => Type[keyof Type];
-export type Dispatcher<Type = any> = (action: Action<Type> | Action<Type>) => void;
-export type StoreEventHandler<Type = any> = (eventId: string, action: any, states: Type, state: OneOnly<Type, keyof Type>) => void;
+export type Dispatcher<Type = any> = (action: Action<Type>) => void;
+export type BatchDispatcher<Type = any> = (actions: ActionBatch<Type>) => void;
+export type StoreEventPayload<Type = any> = {
+    states: Type;
+    eventStamp: string;
+} & {
+    isBatch: false;
+    stateId: string;
+    action: Action<Type>;
+    prevState: OneOnly<Type, keyof Type>;
+} & {
+    isBatch: true;
+    batchStateIds: string[]
+    batchActions: ActionBatch<Type>;
+    prevStates: Type;
+}
+export type StoreEventHandler<Type = any> = (eventId: string, payload: StoreEventPayload, ...args: any[]) => void;
 
 export interface Store<Type = Record<string,any>> {
     /**
@@ -27,6 +42,14 @@ export interface Store<Type = Record<string,any>> {
      * @param stateId 
      */
     getDispatcher(stateId: string): Dispatcher;
+    /**
+     * Gets the bulk dispatcher. The semantics are as follows:
+     * 
+     * - for any stateId changed 1+ times, emit a single event for that
+     * - each `stateIds` value is simply `[stateId]`
+     *   - FUTURE: include every stateId updated? 
+     */
+    getBatchDispatcher(): BatchDispatcher;
     /**
      * Get notified of store events. For typical state updates, you can simply listen
      * for `stateId` as the `eventId`. Other `eventIds` TBD.
@@ -47,6 +70,11 @@ export class StoreImpl<Type = Record<string,any>> implements Store<Type>  {
         this.states = _initState;
     }
 
+    eventCount = 0;
+    __makeEventStamp() {
+        return `${++this.eventCount}-${new Date().toISOString()}`
+    }
+
     getState(stateId: string) {
         if (!this.states[stateId]) {
             throw new Error(`no state set: ${stateId}`)
@@ -62,31 +90,79 @@ export class StoreImpl<Type = Record<string,any>> implements Store<Type>  {
         this.reducers[stateId] = reducer;
     };
 
-    __dispatch(stateId: string, action: Action) {
-        const reducer = this.reducers[stateId];
+    applyReducer(stateId: string, action: Action, state: any) {
+        return this.reducers[stateId](stateId, action, this.states);
+    }
+
+    dispatch(stateId: string, action: Action) {
         const prevState = {...this.states[stateId]};
-        const newState = reducer(stateId, action, this.states);
+        const newState = this.applyReducer(stateId, action, prevState);
         if (newState === prevState) {
             return;
         }
 
         this.states[stateId] = newState;
-        this.emitter.emit(stateId, stateId, action, this.states, prevState);
+        this.emitter.emit(stateId, stateId, {
+            eventStamp: this.__makeEventStamp(),
+            states: this.states,
+            isBatch: false,
+            stateId,
+            action,
+            prevState
+        } as StoreEventPayload);
     }
 
-    getDispatcher(stateId: string): Dispatcher {
+    getDispatcher(stateId: string): Dispatcher<Type> {
         if (!this.reducers[stateId]) {
             throw new Error(`no reducer set: ${stateId}`);
         }
         return (action: Action) => {
-            this.__dispatch(stateId, action);
+            this.dispatch(stateId, action);
+        }
+    }
+
+    batchDispatch(actions: ActionBatch) {
+        const stateIdsChecked: Record<string,boolean> = {};
+        const prevStates = {...this.states};
+        const newStates = {...this.states};
+
+        for (const {stateId, action} of actions) {
+            if (!this.reducers[stateId]) {
+                throw new Error(`no reducer set: ${stateId}`);
+            }
+
+            const newState = this.applyReducer(stateId, action, newStates);
+            if (newState !== prevStates[stateId]) {
+                stateIdsChecked[stateId] = true;
+                newStates[stateId] = newState;
+            }
+        }
+
+        this.states = newStates;
+        const stateIds = Object.keys(stateIdsChecked);
+        const eventStamp = this.__makeEventStamp();
+        for (const stateId of stateIds) {
+            this.emitter.emit(stateId, stateId, {
+                eventStamp,
+                states: newStates,
+                isBatch: true,
+                batchStateIds: stateIds,
+                batchActions: actions,
+                prevStates
+            } as StoreEventPayload)
+        }
+    }
+
+    getBatchDispatcher(): BatchDispatcher<Type> {
+        return (actions: ActionBatch) => {
+            this.batchDispatch(actions);
         }
     }
 
     listenFor(eventId: string, listener: StoreEventHandler) {
-        this.emitter.addListener(eventId, listener);
+        this.emitter.addListener(eventId, listener as ((e:string, ...args: any[]) => void));
         return () => {
-            this.emitter.removeListener(eventId, listener);
+            this.emitter.removeListener(eventId, listener as ((e:string, ...args: any[]) => void));
         }
     }
 }
